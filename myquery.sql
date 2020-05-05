f44712fe5a95d23da364eff55ece19b0b0baff61
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-05-05 08:48:27 +0700

    init commit

---
+++ myquery.sql
@@ -0,0 +1,10 @@
+select customers.customerNumber, customerName,phone,paymentDate, amount 
+from customers 
+inner join payments 
+on customers.customerNumber = payments.customerNumber 
+where city = 'Las Vegas';
+
+select customers.customerNumber , customers.customerName, orders.orderNumber, orders.status
+from customers
+left join orders
+on customers.customerNumber = orders.customerNumber;
